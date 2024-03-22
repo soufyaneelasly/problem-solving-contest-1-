ba0fd9580e5455084f4ca6d10aa5c8f395d25757
soufyaneelasly <elasslysoufyane@gmail.com>
2024-03-22 16:32:35 +0000

    A

---
+++ A.WinterSale/A.Wintersale.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+
+
+    
+    float d,rs,p;
+    
+
+cin >>d;
+cin >>p;
+rs=(p*100)/(100-d);
+cout<<fixed<<setprecision(2)<<rs<<endl;
+
+}
