477693f245b0cf999e930733169273d58007651d
soufyaneelasly <elasslysoufyane@gmail.com>
2024-03-23 23:55:17 +0000

    E

---
+++ E.intervalsweep/Eintervals.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std ;
+
+int main(){
+int a,b;
+cin>>a;
+cin>>b;
+int d;
+d=a-b;
+
+if ((d==1 || d==-1 || d==0 )&& a*b!=0){
+
+cout<<"YES"<<endl;
+}else{
+cout<<"NO"<<endl;
+}
+
+}
+    
